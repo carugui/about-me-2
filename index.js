d2de372567c18b957d9642828c99b837c46183de
Carla C <carla.cc.cg@hotmail.com>
2023-01-05 01:06:44 +0100

    Boton inicio aparece y desaparece y hace scroll

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const scrollContainer = document.querySelector("main");
+let mybutton = document.getElementById("button");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-});
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.classList.add('show');
+  } else {
+    mybutton.classList.remove('show');
+  }
+}
+// Cuando hagas click en el botón, subirá a arriba de la página
+mybutton.addEventListener("click", topFunction);
+
+function topFunction() {
+  document.body.scrollTop = 0; //Safari
+  document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
+}
