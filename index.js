72062b1165711d56125d2c37edeccc0005a11b25
Carla C <carla.cc.cg@hotmail.com>
2023-01-09 01:34:35 +0100

    customizado scrollbar, mejorado scroll en X e Y

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 let mybutton = document.getElementById("button");
+let mynav = document.getElementById("nav");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(), scrollNav()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     mybutton.classList.add('show');
   } else {
     mybutton.classList.remove('show');
+  }
+}
+
+function scrollNav() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mynav.classList.add('nav-green');
+  } else {
+    mynav.classList.remove('nav-green');
   }
 }
 // Cuando hagas click en el botón, subirá a arriba de la página
@@ -16,3 +25,12 @@ function topFunction() {
   document.body.scrollTop = 0; //Safari
   document.documentElement.scrollTop = 0; //Chrome, Firefox, IE and Opera
 }
+
+const scrollContainer = document.querySelector("main");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  if (window.innerWidth > 960) {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+  }
+});
